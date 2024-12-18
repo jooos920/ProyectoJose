136302e88a4f2c59e4cad0c3d9cc68b4e92dfa15
Jose Roberto Nieto Blanco <al222310515@gmail.com>
2024-12-18 09:54:10 -0600

    Middleware creado

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -19,7 +19,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href={{route('dashboard')}}>Inicio</a>
+            {{--<a class="navbar-brand" href={{route('dashboard')}}>Inicio</a>--}}
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -30,26 +30,26 @@
                         <a class="nav-link" href="{{ route('users.index') }}">Usuarios</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('productos.index') }}">Productos</a>
+                        <a class="nav-link" href="">Productos</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('categorias.index') }}">Categorias</a>
+                        <a class="nav-link" href="">Categorias</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('proveedores.index') }}">Proveedores</a>
+                        <a class="nav-link" href="">Proveedores</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                             data-bs-toggle="dropdown" aria-expanded="false">
-                            {{ auth()->user()->name }}
+                            {{---{{ auth()->user()->name }}--}}
                         </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        {{--<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <li><a href="{{ route('users.show', auth()->user()->id) }}" class="dropdown-item">Ver
                                     Información</a></li>
                             <li>
                                 <a href="{{route('logout')}}" class="dropdown-item">Cerrar Sesión</a>
                             </li>
-                        </ul>
+                        </ul>--}}
                     </li>
 
                 </ul>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,42 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductosController;
+use App\Http\Controllers\InicioController;
+use App\Http\Controllers\UsuariosController;
+use App\Http\Middleware\AdminIsAuthenticated;
+use App\Http\Middleware\UserIsAuthenticated;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::middleware(AdminIsAuthenticated::class)->group(function () {
+    Route::get('/dashboard', [InicioController::class, 'dashboard'])->name('dashboard');
+    // Rutas Productos
+    Route::get('/productos', [ProductosController::class, 'index'])->name('productos.index');
+    Route::get('/crear_producto', [ProductosController::class, 'create'])->name('productos.create');
+    Route::post('/crear_producto', [ProductosController::class, 'store'])->name('productos.store');
+    Route::get('/ver_producto/{producto}', [ProductosController::class, 'show'])->name('productos.show');
+    Route::get('/editar_producto/{producto}', [ProductosController::class, 'edit'])->name('productos.edit');
+    Route::post('/editar_producto/{producto}', [ProductosController::class, 'update'])->name('productos.update');
+    Route::get('/eliminar_producto/{id}', [ProductosController::class, 'destroy'])->name('productos.destroy');
 
-Route::get('/', function () {
-    return view('welcome');
+    // Rutas Usuarios
+    Route::get('/usuarios', [UsuariosController::class, 'index'])->name('users.index');
+    Route::get('/crear_usuario', [UsuariosController::class, 'create'])->name('users.create');
+    Route::post('/crear_usuario', [UsuariosController::class, 'store'])->name('users.store');
+    Route::get('/editarusuario/{user}', [UsuariosController::class, 'admin_edit'])->name('users.admin.edit');
+Route::post('/editarusuario/{user}', [UsuariosController::class, 'admin_update'])->name('users.admin.update');
+
 });
+
+Route::middleware(UserIsAuthenticated::class)->group(function () {
+    Route::get('/', [InicioController::class, 'inicio'])->name('inicio');
+});
+Route::get('/login', [AuthController::class, 'showLogin'])->name('verLogin');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/registrar', [AuthController::class, 'create'])->name('signup');
+Route::post('/registrar', [AuthController::class, 'store'])->name('signup.store');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/ver_informacion/{user}', [UsuariosController::class, 'show'])->name('users.show');
+Route::get('/editar_usuario/{user}', [UsuariosController::class, 'edit'])->name('users.edit');
+Route::post('/editar_usuario/{user}', [UsuariosController::class, 'update'])->name('users.update');
+Route::get('/eliminar_usuario/{id}', [UsuariosController::class, 'destroy'])->name('users.destroy');
