fbde14147d834a1e6714855d658aa2312da15f2a
Jose Roberto Nieto Blanco <al222310515@gmail.com>
2024-12-18 09:00:37 -0600

    Controlador, modelo y migración modificadas

--- database/migrations/2024_12_18_034733_create_usuarios_table.php
+++ database/migrations/2024_12_18_034733_create_usuarios_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->integer('status')->default(1);
+            $table->string('rol')->default('usuario');
             $table->timestamps();
         });
     }
